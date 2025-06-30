c6c3efc9ecee91590ae0b3b4969feeba74018f8e
Abhishek <Abhishekraut8697@gmail.com>
2025-06-30 18:20:22 +0530

    building scalable chat app

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,6 +1,8 @@
+import styles from './page.module.css'
+
 export default function Home() {
   return(
-    <div>
+    <div id='chat-app' className={styles.container}>
       <h1>chat app</h1>
     </div>
   )
