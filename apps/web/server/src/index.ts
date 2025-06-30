85c975123fc3c92694a96d53c9fa6087c275afb5
Abhishek <Abhishekraut8697@gmail.com>
2025-07-01 01:51:06 +0530

    connect socket layer with http

--- apps/web/server/src/index.ts
+++ apps/web/server/src/index.ts
@@ -1,12 +1,29 @@
 import express from "express";
-import dotenv from 'dotenv';
-dotenv.config(); 
+import dotenv from "dotenv";
+import { createServer } from "http";
+import socketServer from "./services/socket";
+const PORT = process.env.PORT ? process.env.PORT : 8002;
+
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const httpServer = createServer(app);
+
+const socket = new socketServer();
+socket.io.attach(httpServer);
+socket.intiListener();
 
 
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log("server is listening on port", PORT);
+});
+
+process.on("SIGINT", () => {
+  console.log("🛑 Gracefully shutting down...");
+  httpServer.close(() => {
+    console.log("✅ HTTP server closed");
+    process.exit(0);
+  });
+  
 });
--- apps/web/server/src/services/socket.ts
+++ apps/web/server/src/services/socket.ts
@@ -1,16 +1,29 @@
-import {Server} from "socket.io"
+import { Server } from "socket.io";
 
-class socketServer{
-    private _io : Server;
-    constructor(){
-        console.log("init socket server")
-        this._io = new Server()
-    }
+class socketServer {
+  private _io: Server;
+  constructor() {
+    console.log("init socket server");
+    this._io = new Server();
+  }
 
-    get io(){
-        return this._io
-    }
+  public intiListener() {
+    console.log("intiListener method activated")
+    const io = this._io;
+
+    io.on("connect", (socket) => {
+      console.log("new socket connected", socket.id);
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        // handle the message event here
+        console.log("Received message:", message);
+      });
+    });
+    
+  }
+
+  get io() {
+    return this._io;
+  }
 }
 
-export default socketServer
-
+export default socketServer;
